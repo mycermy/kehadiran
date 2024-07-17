2efb8199b4aa017cc69e44a5f0c80fee207e5118
Abu Anas <mycermy@gmail.com>
2024-07-17 20:52:56 +0800

    undo kubu root

--- app/Orchid/Screens/Role/RoleListScreen.php
+++ app/Orchid/Screens/Role/RoleListScreen.php
@@ -20,15 +20,19 @@ class RoleListScreen extends Screen
      */
     public function query(): iterable
     {
-        $id = auth()->id();
-        
         return [
-            'roles' => Role::
-                when($id != 1, function (Builder $query) {
-                    $query->whereNot('id', 1); 
-                })
-                ->filters()->defaultSort('id', 'desc')->paginate(),
+            'roles' => Role::filters()->defaultSort('id', 'desc')->paginate(),
         ];
+
+        // $id = auth()->id();
+        
+        // return [
+        //     'roles' => Role::
+        //         when($id != 1, function (Builder $query) {
+        //             $query->whereNot('id', 1); 
+        //         })
+        //         ->filters()->defaultSort('id', 'desc')->paginate(),
+        // ];
     }
 
     /**
